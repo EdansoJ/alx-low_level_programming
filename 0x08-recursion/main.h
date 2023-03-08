8b2cee48dff27c077f3eb8d93b09eac2c1f0a948
root <dansoeddy44@gmail.com>
2023-03-08 13:55:13 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,6 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
